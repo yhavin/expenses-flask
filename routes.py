dace24eaf68f17aadd480020cda2edb68b6e206e
yhavin <y.havin@gmail.com>
2023-06-08 20:43:08 +1000

    Add expense function

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from app import app, db
 from flask import request, jsonify
 from models import User, Expense
+from datetime import datetime
 
 
 @app.route("/")
@@ -24,3 +25,16 @@ def register():
     db.session.commit()
 
     return jsonify({"message": f"User {username} registered successfully"}), 201
+
+
+@app.route("/expenses", methods=["POST"])
+def add_expense():
+    data = request.get_json()
+    date = datetime.strptime(data["date"], "%Y-%m-%d").date()
+    user_id = 123456
+    expense = Expense(date=date, description=data["description"], category=data["category"], amount=data["amount"], user_id=user_id)
+
+    db.session.add(expense)
+    db.session.commit()
+
+    return jsonify({"message": "Expense saved successfully"})
