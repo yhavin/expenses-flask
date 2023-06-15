2d477e5613486c77101058bf70a06292ce26bda3
yhavin <y.havin@gmail.com>
2023-06-15 17:34:25 +1000

    Added UserMixin

--- flask/models.py
+++ flask/models.py
@@ -1,8 +1,9 @@
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), index=True, unique=True)
     password_hash = db.Column(db.String(128))
